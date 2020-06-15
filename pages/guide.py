d2de662e3021b603b0bfb83a294ffde20fe7e988
Akarsh Saxena <akarsh.saxena.as@gmail.com>
2020-06-15 22:43:00 +0530

    Create guide.py

---
+++ pages/guide.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import utils
+
+def write(_):
+
+    cache = utils.get_static_store()
+    cache['copy'] = None
+
+    st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/International_Morse_Code.svg/800px-International_Morse_Code.svg.png', use_column_width=True)
+
+    st.info("""
+
+        For list of all supported characters, visit [Wikipedia]('https://en.wikipedia.org/wiki/Morse_code#Letters,_numbers,_punctuation,_prosigns_for_Morse_code_and_non-English_variants')
+
+    """)
+
+if __name__ == '__main__':
+    write(_)
