460a29e36ccee3a38ba7b0859f7729ac693b8ad9
Akarsh Saxena <akarsh.saxena.as@gmail.com>
2020-06-15 21:29:39 +0530

    Create home.py

---
+++ pages/home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+def write(_):
+
+    st.image("https://hackster.imgix.net/uploads/attachments/583295/blob_H3sQ6UPdXT.blob?auto=compress%2Cformat&w=900&h=675&fit=min", use_column_width=True)
+
+    st.markdown("""
+        <br />
+
+        # MorseCrypter
+        ## Encode any text (even from images) to morse code!
+
+        <br />
+    """, unsafe_allow_html=True)
+
+    st.success("""
+        ## Usage
+
+        ***From the left navigation menu, select the source for the input.***
+    """)
+
+if __name__ == "__main__":
+    write(_)
